831d9aa97c4c8068a3b388b7f3b07bd8683990bd
AOlefirenko <olef_and@ukr.net>
2022-05-15 00:46:30 -0400

    tokun

--- orbitez/pages/api/deploy_orbitez_do.js
+++ orbitez/pages/api/deploy_orbitez_do.js
@@ -2,7 +2,7 @@
 import { DigitalOceanAccount } from '../../digitalocean/digitalocean_account' 
 import { Region } from '../../digitalocean/model/digitalocean';
 
-const TOKEN = 'dop_v1_566c783344683aa6eea4135bb4e5660e369199e78294b549bfaa298882ede47b'
+const TOKEN = ''
 const REGION = 'nyc3'
 
 export default function handler(req, res) {
