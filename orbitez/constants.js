c1021510e229e75fd67b28d40faa3dc83f9ac06b
AOlefirenko <olef_and@ukr.net>
2022-05-22 16:35:13 -0400

    const

--- orbitez/constants.js
+++ orbitez/constants.js
@@ -1,4 +1,4 @@
 export const RPC_URL = 'https://rpc.ithacanet.teztnets.xyz'
-export const CONTRACT_ADDRESS = 'KT1M1EYCLu9dkGBZm8z5BWJDCB3QCydAyjf1'
+export const CONTRACT_ADDRESS = 'KT1K7vkeX6CWCEJuUDJkjGBMT3eCDf77C3gA'
 export const NFT_ADDRESS = 'KT1QUPuQYpqmfUZ7MtE9AhJ11n7ce1mKyyMB'
 export const NFT_CONTRACT_ADDRESS = 'KT1AEVuykWeuuFX7QkEAMNtffzwhe1Z98hJS'
